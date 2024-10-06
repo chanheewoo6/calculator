f734b33d2fae9558d380b003a4e1e9a9f3f48371
chanhee <woochanhee02@gmail.com>
2024-10-06 17:50:26 +0900

    last commit

--- chartcalculator/Graphs/graphManager.js
+++ chartcalculator/Graphs/graphManager.js
@@ -90,21 +90,23 @@ function createGraphFromInput(graphType) {
 
 function displaySelectedTopic(topic) {
   // 선택된 주제를 화면에 표시
-  document.getElementById('selectedTopic').textContent = `선택된 주제: ${topic}`;
+  document.getElementById('selectedTopic').textContent = `선택된 그래프: ${topic}`;
 }
 
 // 각 버튼에 클릭 이벤트를 추가하여 그래프를 생성
-document.getElementById('BarGraph').addEventListener('click', function() {
-  displaySelectedTopic('막대그래프');
-  createGraphFromInput('bar');
-});
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('BarGraph').addEventListener('click', function() {
+    displaySelectedTopic('막대그래프');
+    createGraphFromInput('bar');
+  });
 
-document.getElementById('lineGraph').addEventListener('click', function() {
-  displaySelectedTopic('꺾은선그래프');
-  createGraphFromInput('line');
-});
+  document.getElementById('lineGraph').addEventListener('click', function() {
+    displaySelectedTopic('꺾은선그래프');
+    createGraphFromInput('line');
+  });
 
-document.getElementById('circleGraph').addEventListener('click', function() {
-  displaySelectedTopic('원그래프');
-  createGraphFromInput('pie');
+  document.getElementById('circleGraph').addEventListener('click', function() {
+    displaySelectedTopic('원그래프');
+    createGraphFromInput('pie');
+  });
 });
